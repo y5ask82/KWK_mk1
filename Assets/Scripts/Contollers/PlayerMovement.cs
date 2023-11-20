f4eb1622812f9fb1e07249cb6f674f1ab44325c6
Kyungwon Kim <gr1996@naver.com>
2023-11-20 14:40:05 +0900

    플레이어 애니메이션
    플레이어 애니메이션, 아이들, 점프, 걷기

--- Assets/Scripts/Contollers/PlayerMovement.cs
+++ Assets/Scripts/Contollers/PlayerMovement.cs
@@ -23,8 +23,12 @@ public class PlayerMovement : MonoBehaviour
     int jumpCnt;
 
     bool isGround;
+
+    public Animator animator;
+
     private void Start()
     {
+        animator = GetComponent<Animator>();
         myrigidbody = GetComponent<Rigidbody2D>();
         jumpCnt = jumpCount;
     }
@@ -34,11 +38,17 @@ public class PlayerMovement : MonoBehaviour
         isGround = Physics2D.OverlapCircle(player.position, checkRadius, islayer);
         if(isGround == true && Input.GetKeyDown(KeyCode.Space) && jumpCnt > 0)
         {
+            animator.SetTrigger("Jump");
             myrigidbody.velocity = Vector2.up * power;
         }
         if (isGround == false && Input.GetKeyDown(KeyCode.Space) && jumpCnt > 0)
         {
             myrigidbody.velocity = Vector2.up * power;
+        }
+        if (isGround && jumpCnt != jumpCount)
+        {
+            myrigidbody.velocity = Vector2.zero;
+            animator.SetTrigger("isground");
         }
         if(Input.GetKey(KeyCode.Space))
         {
@@ -54,14 +64,24 @@ public class PlayerMovement : MonoBehaviour
         float hor = Input.GetAxis("Horizontal");
         myrigidbody.velocity = new Vector2(hor * 3, myrigidbody.velocity.y);
 
-        if(hor > 0)
+        if (hor > 0)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
+            animator.SetBool("Walk", true);
+            animator.SetBool("runNIdle", true);
         }
 
         else if (hor < 0)
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
+            animator.SetBool("Walk", true);
+            animator.SetBool("runNIdle", true);
+        }
+
+        else
+        {
+            animator.SetBool("Walk", false);
+            animator.SetBool("runNIdle", false);
         }
     }
 }
