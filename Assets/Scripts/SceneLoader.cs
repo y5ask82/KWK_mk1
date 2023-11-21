295852373c3ef1386a0b5ff5b7bd870057a1164b
Kyungwon Kim <gr1996@naver.com>
2023-11-21 11:09:36 +0900

    스타트씬 생성
    스타스씬 생성, 버튼만들기(시작,이어하기,옵션,사운드on/off, 뒤로가기, 종료), 버튼ui(버튼 커짐)

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Slider progressbar;
+    public Text loadtext;
+    public static string loadScene;
+    public static int loadType;
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    public static void LoadSceneHandle(string _name, int _loadType)
+    {
+        loadScene = _name;
+        loadType = _loadType;
+        SceneManager.LoadScene("Loading");
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(loadScene);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            yield return null;
+
+            if (loadType == 0)
+                Debug.Log("새게임");
+            else if (loadType == 1)
+                Debug.Log("헌게임");
+
+            if (progressbar.value < 0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
+            }
+            else if (operation.progress >= 0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+            }
+        }
+    }
+}
