6d9cd1f857d7cd643141a76b4f3960353ff6ffbb
Kyungwon Kim <gr1996@naver.com>
2023-12-11 17:12:24 +0900

    적3 생성
    적3 생성

---
+++ Assets/Scripts/Enemy/Enemy3Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3Move : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public int nextMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        Invoke("Think", 5);
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rayHit.collider == null)
+        {
+            nextMove *= -1;
+            CancelInvoke();
+            Invoke("Think", 5);
+        }
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        Invoke("Think", 5);
+    }
+}
