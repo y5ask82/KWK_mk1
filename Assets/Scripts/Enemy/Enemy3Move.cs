bf483cd55f66e2d342c610a2b16ee72ed2fd4c80
Kyungwon Kim <gr1996@naver.com>
2023-12-13 10:19:50 +0900

    적3
    적3 생성

--- Assets/Scripts/Enemy/Enemy3Move.cs
+++ Assets/Scripts/Enemy/Enemy3Move.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Enemy3Move : MonoBehaviour
 {
     Rigidbody2D rigid;
+    
+    SpriteRenderer spriteRenderer;
+
     public int nextMove;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         Invoke("Think", 5);
     }
@@ -18,16 +23,14 @@ public class Enemy3Move : MonoBehaviour
     {
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
-        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y);
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
 
-        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Ground"));
 
         if (rayHit.collider == null)
         {
-            nextMove *= -1;
-            CancelInvoke();
-            Invoke("Think", 5);
+            Turn();
         }
     }
 
@@ -35,6 +38,21 @@ public class Enemy3Move : MonoBehaviour
     {
         nextMove = Random.Range(-1, 2);
 
-        Invoke("Think", 5);
+        
+
+        if(nextMove != 0)
+        spriteRenderer.flipX = nextMove == 1;
+
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 2);
     }
 }
